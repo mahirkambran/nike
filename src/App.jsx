f1643698f8987ba67a24f2322a588e56dbecc7d4
MUHAMMED MAHIR K <mahirkambran@gmail.com>
2024-08-25 14:50:45 +0530

    nike initial commit(hero section and nav bar completed)

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Hero from '../src/sections/Hero'
+import PopularProducts from '../src/sections/PopularProducts'
+import SuperQuality from '../src/sections/SuperQuality'
+import Services from '../src/sections/Services'
+import SpecialOffer from '../src/sections/SpecialOffer'
+import CustomerReviews from '../src/sections/CustomerReviews'
+import Subscribe from '../src/sections/Subscribe'
+import Footer from '../src/sections/Footer'
+import Nav from './components/Nav'
+
+
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+  <section className="xl:padding-l wide:padding-r padding-b"> 
+    <Hero />
+  </section>
+  <section className="padding">
+    <PopularProducts />
+  </section>
+  <section className="padding">
+    <SuperQuality />
+  </section>
+  <section className="padding">
+    <Services />
+  </section>
+  <section className="padding">
+    <SpecialOffer />
+  </section>
+  <section className="bg-pale-blue padding">
+    <CustomerReviews />
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full">
+    <Subscribe />
+  </section>
+  <section className="bg-black padding padding-x padding-t pb-8">
+    <Footer />
+  </section>
+  </main>
+);
+
+export default App;
